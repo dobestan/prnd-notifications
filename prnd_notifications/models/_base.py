e670a7c145ff6787d75df57ec405d6fcb996cf9e
dobestan <dobestan@gmail.com>
2015-06-30 13:57:57 +0900

    update prnd_notifications : auto include notifications models including SlackNotification, SmsNotification, EmailNotification

--- prnd_notifications/models/_base.py
+++ prnd_notifications/models/_base.py
@@ -11,7 +11,7 @@ class AbstractBaseNotification(models.Model):
 
     sender = models.CharField(max_length=255, blank=True, null=True)
     receiver = models.CharField(max_length=255, blank=True, null=True)
-    content = models.CharField(max_length=255, blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
 
     status_code = models.PositiveSmallIntegerField(blank=True, null=True)
     response_content = models.TextField(blank=True, null=True)
